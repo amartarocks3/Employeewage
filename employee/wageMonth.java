f7092956db05d08adbeefe046114bee755c76db5
amartarocks3 <amartarocks3@gmail.com>
2022-04-05 08:29:47 +0530

    Added : total wage for month

---
+++ employee/wageMonth.java
@@ -0,0 +1,35 @@
+package employee;
+
+import java.util.Random;
+
+public class wageMonth {
+
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int empHrs = 0;
+			int totalWage=0;
+			int empWage = 0;
+			for(int i=0;i<20;i++) {
+			Random rand = new Random();
+			int random1 = rand.nextInt();
+			int empCheck = random1%3;
+			switch(empCheck) {
+				case IS_FULL_TIME:empHrs = 8 ;
+				break;
+				case IS_PART_TIME:empHrs =4;
+				break;
+				default :empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalWage +=empWage;
+			System.out.println("Emp Wage: " + empWage);
+			
+		}
+			System.out.println("Total Wage of month is = "+totalWage);
+	}
+
+}
